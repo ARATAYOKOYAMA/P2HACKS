29636f3adc9ebbb8626117322386239d94980336
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2018-10-16 13:38:42 +0900

    Initial Commit

---
+++ P2HAKCS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P2HAKCS
+//
+//  Created by 横山新 on 2018/10/16.
+//  Copyright © 2018年 横山新. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ P2HAKCSTests/P2HAKCSTests.swift
@@ -0,0 +1,34 @@
+//
+//  P2HAKCSTests.swift
+//  P2HAKCSTests
+//
+//  Created by 横山新 on 2018/10/16.
+//  Copyright © 2018年 横山新. All rights reserved.
+//
+
+import XCTest
+@testable import P2HAKCS
+
+class P2HAKCSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
